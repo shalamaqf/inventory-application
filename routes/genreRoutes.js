9ea28ae29fb14bcfb36741386ab4682e715d8a32
Shalama <shalamafadillaa@gmail.com>
2026-08-05 16:13:05 +0700

    Add routes for genre route path

---
+++ routes/genreRoutes.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const genreRouter = Router();
+
+
+genreRouter.get("/", (req, res) => {
+    res.send("this is home page");
+})
+
+genreRouter.get("/genre/:genre_name", (req, res) => {
+    res.send("this is album page by genre");
+})
+
+genreRouter.get("/genre/add-genre", (req, res) => {
+    res.send("this is form to add genre");
+})
+
+genreRouter.get("/genre/update-genre/:id", (req, res) => {
+    res.send("this is form to update genre");
+})
+
+genreRouter.get("/genre/deleted-genre/:id", (req, res) => {
+    res.send("this is delete genre page");
+})
+
+module.exports = genreRouter;
