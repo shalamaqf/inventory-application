d51a4bbd0f6fcb651908e454b8027a2c864044df
Shalama <shalamafadillaa@gmail.com>
2026-08-06 13:19:05 +0700

    Fix album path and export it

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -2,26 +2,28 @@ const { Router } = require("express");
 const albumRouter = Router();
 
 
-albumRouter.get("/genre/:genre_name/search/:album_name", (req, res) => {
+albumRouter.get("/search/:album_name", (req, res) => {
     res.send("this is album name");
 })
 
-albumRouter.get("/genre/:genre_name/add-album", (req, res) => {
+albumRouter.get("/add-album", (req, res) => {
     res.send("this is form to add album");
 })
 
-albumRouter.get("/genre/:genre_name/update-album", (req, res) => {
+albumRouter.get("/update-album", (req, res) => {
     res.send("this is form to update album");
 })
 
-albumRouter.post("/genre/:genre_name/add-album", (req, res) => {
+albumRouter.post("/add-album", (req, res) => {
     res.send("adding album...");
 })
 
-albumRouter.post("/genre/:genre_name/update-album", (req, res) => {
+albumRouter.post("/update-album", (req, res) => {
     res.send("updating album...");
 })
 
-albumRouter.post("/genre/:genre_name/deleted-album/:id", (req, res) => {
+albumRouter.post("/deleted-album/:id", (req, res) => {
     res.redirect("/genre/:genre_name");
 })
+
+module.exports = albumRouter;
