5956609df68bf940ea537a9b8e7d9ad91a71886a
Shalama <shalamafadillaa@gmail.com>
2026-08-11 13:21:05 +0700

    Create getArtistById async function
    Create query to get artist by its id.

--- db/artistQueries.js
+++ db/artistQueries.js
@@ -4,3 +4,8 @@ async function getArtistByName(name) {
     const { rows } = await pool.query("SELECT * FROM artist WHERE name = $1", [name]);
     return rows[0];
 }
+
+async function getArtistById(id) {
+    const { rows } = await pool.query("SELECT * FROM artist WHERE id = $1", [id]);
+    return rows[0];
+}
