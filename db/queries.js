d81943362a018cfcd3c15f91eec21b7dbf65215d
Shalama <shalamafadillaa@gmail.com>
2026-08-11 10:28:52 +0700

    Create addGenre async function
    Create query to add new genre to the genre table and return new genre's id.

--- db/queries.js
+++ db/queries.js
@@ -4,3 +4,8 @@ async function getAllGenres() {
     const { rows } = await pool.query("SELECT * FROM genre");
     return rows;
 }
+
+async function addGenre(name) {
+    const { rows } = await pool.query("INSERT INTO genre (name) VALUES ($1) RETURNING *", [name]);
+    return rows[0];
+}
