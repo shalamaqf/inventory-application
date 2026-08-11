8dd9569e9f6d14732b81e22699c2b140b1de0c95
Shalama <shalamafadillaa@gmail.com>
2026-08-11 10:45:13 +0700

    Create getGenreById async function
    Create query to get genre by its id.

--- db/queries.js
+++ db/queries.js
@@ -14,3 +14,8 @@ async function updateGenre(name, id) {
     const { rows } = await pool.query("UPDATE genre SET name = $1 WHERE id = $2 RETURNING *", [name, id]);
     return rows[0];
 }
+
+async function getGenreById(id) {
+    const { rows } = await pool.query("SELECT * FROM genre WHERE id = $1", [id]);
+    return rows[0];
+}
