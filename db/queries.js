9920cb2899505a0a3b072a295adda3d21b40f884
Shalama <shalamafadillaa@gmail.com>
2026-08-11 10:39:01 +0700

    Create updateGenre async function
    Create query to update genre based on its id.

--- db/queries.js
+++ db/queries.js
@@ -9,3 +9,8 @@ async function addGenre(name) {
     const { rows } = await pool.query("INSERT INTO genre (name) VALUES ($1) RETURNING *", [name]);
     return rows[0];
 }
+
+async function updateGenre(name, id) {
+    const { rows } = await pool.query("UPDATE genre SET name = $1 WHERE id = $2 RETURNING *", [name, id]);
+    return rows[0];
+}
