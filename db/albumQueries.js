bc67ec49a92cdf15ca13933dfcfb79dfa02a84a3
Shalama <shalamafadillaa@gmail.com>
2026-08-11 11:19:38 +0700

    Create getAlbumByNameAndGenre async function
    Create query to get album by its name and genre.

--- db/albumQueries.js
+++ db/albumQueries.js
@@ -4,3 +4,8 @@ async function getAllAlbumsByGenre(genreId) {
     const { rows } = await pool.query("SELECT * FROM album WHERE genre_id = $1", [genreId]);
     return rows;
 }
+
+async function getAlbumByNameAndGenre(name, genreId) {
+    const { rows } = await pool.query("SELECT * FROM album WHERE name = $1 AND genre_id = $2", [name, genreId]);
+    return rows;
+}
