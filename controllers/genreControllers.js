595b19e616003496905b8b2c6084e5ed822abeb3
Shalama <shalamafadillaa@gmail.com>
2026-08-13 10:47:20 +0700

    Create genreListGet async function

--- controllers/genreControllers.js
+++ controllers/genreControllers.js
@@ -1,1 +1,7 @@
 const db = require("../db/genreQueries");
+
+async function genreListGet(req, res) {
+    const genreList = await db.getAllGenres();
+    console.log('Genres: ', genreList);
+    res.render('index', { title: "Genre List", genreList })
+}
