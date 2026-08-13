f7a3b4c9e839f2db27e03a4eccbb1d0513d4c959
Shalama <shalamafadillaa@gmail.com>
2026-08-13 11:58:10 +0700

    Create albumListByGenreGet async function
    Create to render album list by genre.

--- controllers/albumControllers.js
+++ controllers/albumControllers.js
@@ -1,1 +1,21 @@
-const db = require("../db/albumQueries");
+const db_album = require("../db/albumQueries");
+const db_genre = require("../db/genreQueries");
+
+async function albumListByGenreGet(req, res) {
+    const genreName = req.params.genre_name;
+    const genre = await db_genre.getGenreByName(genreName);
+
+    if (!genre) {
+        res.render("404", { title: "404 | Page Not Found" });
+        return;
+    }
+
+    const genreId = genre.id;
+    const albumList = await db_album.getAllAlbumsByGenre(genreId);
+    console.log("Album List: ", albumList);
+    res.render("albumListByGenre", { title: `${genre.name} Album List`, genre, albumList})
+}
+
+module.exports = {
+    albumListByGenreGet
+}
